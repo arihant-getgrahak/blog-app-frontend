d4ea090b70cccae8b19f5c46d229807ba2625290
arihant-getgrahak <arihant.jain@getgrahak.in>
2024-10-17 08:04:00 +0530

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="text-center p-10">
+      <h1>Welcome to our Blog App</h1>
+      <p>This is the homepage.</p>
+      <p>
+        Want to access Blogs?? <Link className="underline text-blue-700" href="/blog">Click Here</Link>
+      </p>
+      <p>To know other Route click here <Link className="underline text-blue-700" href="https://github.com/arihant-getgrahak/blog-app-frontend" target="_blank">Github Link</Link> </p>
     </div>
   );
 }
